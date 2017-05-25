c801ccd3eca653958424ee29bdb50e5d0418ae6e
Daniel Lee <DLee@Daniels-MBP.home>
2017-05-25 17:24:07 -0400

    installing googlemaps pod

---
+++ Pizza Finder/HomescreenVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pizza Finder
+//
+//  Created by Daniel Lee on 5/25/17.
+//  Copyright © 2017 DLEE. All rights reserved.
+//
+
+import UIKit
+
+class HomescreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .red
+    }
+    
+}
+
